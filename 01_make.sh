f3b010b817578fbb5763f3f52a74385e12338969
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2020-07-16 13:34:02 +0300

    more cooking

--- 01_make.sh
+++ 01_make.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-rm -rf build
-mkdir build
 pushd build
-touch here-as-dummy.txt
 cmake ../src
 make
 popd
--- 10_clean.sh
+++ 10_clean.sh
@@ -1,1 +1,5 @@
-rm -rf build
+rm -f build/ascii_invaders
+rm -f build/CMakeCache.txt
+rm -rf build/CMakeFiles/
+rm -f build/Makefile
+rm -f build/cmake_install.cmake
